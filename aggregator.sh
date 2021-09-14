1ecb0223259077c956e95f07965421b16eeab724
Amr Mohammed Kamal <amr.alzehary@gmail.com>
2021-09-14 20:20:49 +0200

    Update aggregator.sh

--- aggregator.sh
+++ aggregator.sh
@@ -1,2 +1,2 @@
-python3 /home/pi/grabserial/grabserial -d /dev/ttyACM0 -b 9600  -o /home/pi/serialtest.json  -e  120
+python3 /home/pi/grabserial/grabserial -d /dev/ttyACM0 -b 9600  -o /home/pi/serialtest.json  -e  31 -m "^{.*"
 curl -H "Content-Type: application/json" -X POST -d "@/home/pi/serialtest.json"  https://farmtopia.farmos.net/farm/sensor/listener/a7a5d8b103e51b03d17cdf70f584ebdf?private_key=6396c8023faf3501d945fea28098442b --verbose
